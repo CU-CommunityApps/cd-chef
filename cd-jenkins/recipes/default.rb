eb4681bb62aeda76c17c9077e0c27e78c1558167
sbower <shawn.bower@gmail.com>
2016-08-29 23:20:21 -0400

    need to restart the docker service to pick up mount

--- cd-jenkins/recipes/default.rb
+++ cd-jenkins/recipes/default.rb
@@ -30,4 +30,9 @@ execute "mount efs directory" do
   command "mount -t nfs4 -o nfsvers=4.1 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).fs-137fb25a.efs.us-east-1.amazonaws.com:/ /var/jenkins_home"
   retries 1
   retry_delay 5
+  not_if "grep -qs '/var/jenkins_home' /proc/mounts"
+end
+
+service 'docker' do
+  action :restart
 end
