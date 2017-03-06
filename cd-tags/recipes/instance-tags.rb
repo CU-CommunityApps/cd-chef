65ea1b18ab1b8015c18fde683fb74c64c0ebf1c9
paul-e-allen <pea1@cornell.edu>
2017-03-06 13:43:32 -0500

    Tags (#3)
    * add instance tags

---
+++ cd-tags/recipes/instance-tags.rb
@@ -0,0 +1,34 @@
+#
+# Cookbook Name:: cd-tags
+# Recipe:: instance-tags
+#
+# Copyright (c) 2016 Cornell University, All Rights Reserved.
+
+include_recipe 'aws'
+
+# Rquires IAM policy something like this:
+
+# {
+#     "Version": "2012-10-17",
+#     "Statement": [
+#         {
+#             "Sid": "Stmt1464916254000",
+#             "Effect": "Allow",
+#             "Action": [
+#                 "ec2:CreateTags",
+#                 "ec2:DeleteTags",
+#                 "ec2:DescribeTags"
+#             ],
+#             "Resource": [
+#                 "*"
+#             ]
+#         }
+#     ]
+# }
+
+node['instance-tags'].each do |tag|
+  aws_resource_tag node['ec2']['instance_id'] do
+    tags(tag['key'] => tag['value'])
+    action :update
+  end
+end
