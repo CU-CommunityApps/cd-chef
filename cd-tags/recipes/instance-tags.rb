c95f83a4864f918d14f0d897098d57375b22d789
Paul Allen <pea1@cornell.edu>
2017-03-06 13:41:59 -0500

    add note about required iam policy for dealing with tags

--- cd-tags/recipes/instance-tags.rb
+++ cd-tags/recipes/instance-tags.rb
@@ -6,6 +6,26 @@
 
 include_recipe 'aws'
 
+# Rquires IAM policy something like this:
+
+# {
+#     "Version": "2012-10-17",
+#     "Statement": [
+#         {
+#             "Sid": "Stmt1464916254000",
+#             "Effect": "Allow",
+#             "Action": [
+#                 "ec2:CreateTags",
+#                 "ec2:DeleteTags",
+#                 "ec2:DescribeTags"
+#             ],
+#             "Resource": [
+#                 "*"
+#             ]
+#         }
+#     ]
+# }
+
 node['instance-tags'].each do |tag|
   aws_resource_tag node['ec2']['instance_id'] do
     tags(tag['key'] => tag['value'])
