603d235f6d9476c9284c097b85c76a264f4be36d
nl85cit <ned.lacelle@cornell.edu>
2017-03-07 17:44:45 -0500

    with tags

--- cd-base/recipes/ecs.rb
+++ cd-base/recipes/ecs.rb
@@ -24,39 +24,39 @@ end
 #  notifies :redeploy, 'docker_container[ecs-agent]'
 #end
 
-docker_container 'ecs-agent' do
-  image 'amazon/amazon-ecs-agent'
+#docker_container 'ecs-agent' do
+#  image 'amazon/amazon-ecs-agent'
 #  tag 'latest'
-  action :run_if_missing
-  detach true
-  port '127.0.0.1:51678:51678'
-  port '127.0.0.1:51678:51679'
-  restart_policy 'on-failure'
-  restart_maximum_retry_count 10
-  env lazy {
-    ::File.open('/etc/ecs/ecs.config').read.split("\n")
-  }
-  binds %w{
-    /var/run/docker.sock:/var/run/docker.sock
-    /var/log/ecs/:/log
-    /var/lib/ecs/data:/data
-    /sys/fs/cgroup:/sys/fs/cgroup:ro
-    /var/run/docker/execdriver/native:/var/lib/docker/execdriver/native:ro
-  }
-end
+#  action :run_if_missing
+#  detach true
+#  port '127.0.0.1:51678:51678'
+#  port '127.0.0.1:51678:51679'
+#  restart_policy 'on-failure'
+#  restart_maximum_retry_count 10
+#  env lazy {
+#    ::File.open('/etc/ecs/ecs.config').read.split("\n")
+#  }
+#  binds %w{
+#    /var/run/docker.sock:/var/run/docker.sock
+#    /var/log/ecs/:/log
+#    /var/lib/ecs/data:/data
+#    /sys/fs/cgroup:/sys/fs/cgroup:ro
+#    /var/run/docker/execdriver/native:/var/lib/docker/execdriver/native:ro
+#  }
+#end
 
-#execute "Install the Amazon ECS agent" do
-#  command ["/usr/bin/docker",
-#           "run",
-#           "--name ecs-agent",
-#           "-d",
-#           "-v /var/log/ecs:/log",
-#           "-v /var/lib/ecs/data:/data",
-#           "-p 127.0.0.1:51678:51678",
-#           "-p 127.0.0.1:51679:51679",
-#           "--env-file /etc/ecs/ecs.config",
+execute "Install the Amazon ECS agent" do
+  command ["/usr/bin/docker",
+           "run",
+           "--name ecs-agent",
+           "-d",
+           "-v /var/log/ecs:/log",
+           "-v /var/lib/ecs/data:/data",
+           "-p 127.0.0.1:51678:51678",
+           "-p 127.0.0.1:51679:51679",
+           "--env-file /etc/ecs/ecs.config",
+           "amazon/amazon-ecs-agent"].join(" ")
 #           "amazon/amazon-ecs-agent:latest"].join(" ")
-#
-#  retries 1
-#  retry_delay 5
-#  end
+  retries 1
+  retry_delay 5
+  end
