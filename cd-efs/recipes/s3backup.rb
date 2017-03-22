c0d92d5cf31bc49b2f2d2d2b63ed18e145000be3
Paul Allen <pea1@cornell.edu>
2017-03-22 14:55:41 -0400

    add cron script file template

--- cd-efs/recipes/s3backup.rb
+++ cd-efs/recipes/s3backup.rb
@@ -1,13 +1,26 @@
 
+package 's3cmd'
+
+template '/root/efs-backup.sh' do
+  source 's3backup/efs-backup.sh.erb'
+  owner 'root'
+  group 'root'
+  mode '0744'
+  variables({
+    :filesystems => node['filesystems']
+  })
+  action :create
+end
+
 node['filesystems'].each do |group|
 
   group['mounts'].each do |efs|
 
     cron "backup-#{efs['efs_id']}" do
-      minute '*/2'
+      minute '*/5'
       home '/'
       command %W{
-        ls -al /mnt/#{group['ad_group_dir_name']}/#{efs['mount_dir_name']} >> /var/log/efs-backup.log
+        ls -al /mnt/#{group['ad_group_dir_name']}/#{efs['mount_dir_name']} >> /var/log/efs-backup.log &&
         date >> /var/log/efs-backup.log
       }.join(' ')
     end
