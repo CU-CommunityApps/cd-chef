2f646811608a1cd89d12258c8e2d075dda993380
paul-e-allen <pea1@cornell.edu>
2017-03-23 09:41:09 -0400

    S3cmd (#5)
    Add cd-efs::s3backups recipe that uses s3cmd support for configuring cron-based backups of EFS to S3.

---
+++ cd-efs/recipes/s3backup.rb
@@ -0,0 +1,23 @@
+
+package 's3cmd'
+
+template '/root/efs-backup.sh' do
+  source 's3backup/efs-backup.sh.erb'
+  owner 'root'
+  group 'root'
+  mode '0744'
+  variables({
+    :filesystems => node['efs_mounts']
+  })
+  action :create
+end
+
+#See existing jobs: cat /var/spool/cron/crontabs/root
+cron "backup-efs" do
+  minute '0'
+  hour '4'
+  day '*'
+  home '/root'
+  command "/root/efs-backup.sh"
+end
+
