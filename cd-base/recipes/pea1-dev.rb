371913b6aa96265108fe1df3f2460911e92d0a7c
Paul Allen <pea1@cornell.edu>
2017-03-29 13:22:19 -0400

    add region back to s3 file pull

--- cd-base/recipes/pea1-dev.rb
+++ cd-base/recipes/pea1-dev.rb
@@ -16,6 +16,7 @@ aws_region = stack['region']
 
 aws_s3_file '/tmp/ofm_idm_linux_11.1.1.7.0_64_disk1_1of1.zip' do
   bucket 'cu-cs-odsee'
+  region aws_region
   remote_path 'ofm_idm_linux_11.1.1.7.0_64_disk1_1of1.zip'
   checksum 'fe3ed97bebcbaa11e9a14e2115c90da369fd1a737a96d25e273eb0a74faa1f27'
   etag 'd4a9f943fb331a3aa9bc41a73050852b-245'
