12fe5d528a89b31da56fc4a8b26f088fa0d907d6
Paul Allen <pea1@cornell.edu>
2017-03-29 14:41:05 -0400

    fix zip file name

--- cd-base/recipes/pea1-dev.rb
+++ cd-base/recipes/pea1-dev.rb
@@ -1,4 +1,5 @@
 
+package 'unzip'
 package 'glibc-devel.i686'
 package 'libstdc++.i686'
 package 'libstdc++-devel.i686'
@@ -14,10 +15,28 @@ package 'libstdc++-devel.i686'
 stack = search('aws_opsworks_stack').first
 aws_region = stack['region']
 
-aws_s3_file '/tmp/ofm_idm_linux_11.1.1.7.0_64_disk1_1of1.zip' do
+aws_s3_file '/tmp/ofm_odsee_linux_11.1.1.7.0_64_disk1_1of1.zip' do
   bucket 'cu-cs-odsee'
   region aws_region
-  remote_path 'ofm_idm_linux_11.1.1.7.0_64_disk1_1of1.zip'
+  remote_path 'ofm_odsee_linux_11.1.1.7.0_64_disk1_1of1.zip'
+  checksum '6a04b778a32fb79c157d38206a63e66418c8c7fe381371e7a74fe9dc1ee788fa'
   use_etag  true
   action :create_if_missing
+end
+
+aws_s3_file '/tmp/UnlimitedJCEPolicyJDK7.zip' do
+  bucket 'cu-cs-odsee'
+  region aws_region
+  remote_path 'UnlimitedJCEPolicyJDK7.zip'
+  use_etag  true
+  action :create_if_missing
+end
+
+
+%w[ /app /app/ldap /app/ldap/ds-7 ].each do |path|
+  directory path do
+    owner 'root'
+    group 'root'
+    mode '0755'
+  end
 end
