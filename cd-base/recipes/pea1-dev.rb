3db8b41bf4e9e660a38ccfcf85d1e71281b592c0
Paul Allen <pea1@cornell.edu>
2017-03-29 13:01:50 -0400

    pull zip from s3

--- cd-base/recipes/pea1-dev.rb
+++ cd-base/recipes/pea1-dev.rb
@@ -1,13 +1,25 @@
-#chef_gem 'i18n' do
-#  compile_time false
-#end
-
-#gem_package 'i18n'
-
-#require 'i18n'
-
-#include_recipe "odsee::install"
 
 package 'glibc-devel.i686'
 package 'libstdc++.i686'
 package 'libstdc++-devel.i686'
+
+# remote_file '/tmp/ofm_idm_linux_11.1.1.7.0_64_disk1_1of1.zip' do
+#   source 'https://s3.amazonaws.com/cu-cs-odsee/ofm_idm_linux_11.1.1.7.0_64_disk1_1of1.zip'
+#   checksum 'fe3ed97bebcbaa11e9a14e2115c90da369fd1a737a96d25e273eb0a74faa1f27'
+#   etag 'd4a9f943fb331a3aa9bc41a73050852b-245'
+#   use_conditional_get true
+#   action :create_if_missing
+# end
+
+stack = search('aws_opsworks_stack').first
+aws_region = stack['region']
+
+aws_s3_file '/tmp/ofm_idm_linux_11.1.1.7.0_64_disk1_1of1.zip' do
+  bucket 'cu-cs-odsee'
+  remote_path 'ofm_idm_linux_11.1.1.7.0_64_disk1_1of1.zip'
+  region aws_region
+  checksum 'fe3ed97bebcbaa11e9a14e2115c90da369fd1a737a96d25e273eb0a74faa1f27'
+  etag 'd4a9f943fb331a3aa9bc41a73050852b-245'
+  use_conditional_get true
+  action :create_if_missing
+end
