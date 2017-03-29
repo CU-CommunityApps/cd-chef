8367daacaeb723e11b750f8db77bd0b7cdc02682
Paul Allen <pea1@cornell.edu>
2017-03-29 16:26:52 -0400

    extend recipe actions

--- cd-base/recipes/pea1-dev.rb
+++ cd-base/recipes/pea1-dev.rb
@@ -4,16 +4,16 @@ package 'glibc-devel.i686'
 package 'libstdc++.i686'
 package 'libstdc++-devel.i686'
 
-# remote_file '/tmp/ofm_idm_linux_11.1.1.7.0_64_disk1_1of1.zip' do
-#   source 'https://s3.amazonaws.com/cu-cs-odsee/ofm_idm_linux_11.1.1.7.0_64_disk1_1of1.zip'
-#   checksum 'fe3ed97bebcbaa11e9a14e2115c90da369fd1a737a96d25e273eb0a74faa1f27'
-#   etag 'd4a9f943fb331a3aa9bc41a73050852b-245'
-#   use_conditional_get true
-#   action :create_if_missing
-# end
-
 stack = search('aws_opsworks_stack').first
 aws_region = stack['region']
+
+%w[ /app /app/ldap /app/ldap/ds-7 ].each do |path|
+  directory '/app/ldap/ds-7' do
+    owner 'root'
+    group 'root'
+    mode '0755'
+  end
+end
 
 aws_s3_file '/tmp/ofm_odsee_linux_11.1.1.7.0_64_disk1_1of1.zip' do
   bucket 'cu-cs-odsee'
@@ -24,6 +24,15 @@ aws_s3_file '/tmp/ofm_odsee_linux_11.1.1.7.0_64_disk1_1of1.zip' do
   action :create_if_missing
 end
 
+execute 'unzip_outer' do
+  command 'unzip /tmp/ofm_odsee_linux_11.1.1.7.0_64_disk1_1of1.zip'
+  cwd '/tmp'
+end
+
+execute 'unzip_inner' do
+  command 'unzip /tmp/ODSEE_ZIP_Distribution/sun-dsee7.zip -d /app/ldap/ds-7/'
+end
+
 aws_s3_file '/tmp/UnlimitedJCEPolicyJDK7.zip' do
   bucket 'cu-cs-odsee'
   region aws_region
@@ -32,11 +41,22 @@ aws_s3_file '/tmp/UnlimitedJCEPolicyJDK7.zip' do
   action :create_if_missing
 end
 
+execute 'unzip_security' do
+  command 'unzip /tmp/UnlimitedJCEPolicyJDK7.zip'
+  cwd '/tmp'
+end
 
-%w[ /app /app/ldap /app/ldap/ds-7 ].each do |path|
-  directory path do
-    owner 'root'
-    group 'root'
-    mode '0755'
+# cd /app/ldap/ds-7/dsee/jre/lib/security
+# sudo mv local_policy.jar local_policy_old.jar
+# sudo mv US_export_policy.jar US_export_policy_old.jar
+# sudo mv ~/UnlimitedJCEPolicyJDK8/local_policy.jar .
+# sudo mv ~/UnlimitedJCEPolicyJDK8/US_export_policy.jar .
+
+%w[ local_policy.jar US_export_policy.jar ].each do |target_file|
+  remote_file target_file do
+    path '/app/ldap/ds-7/dsee/jre/lib/security/'+target_file
+    source 'file:///tmp/UnlimitedJCEPolicy/'+target_file
   end
 end
+
+
