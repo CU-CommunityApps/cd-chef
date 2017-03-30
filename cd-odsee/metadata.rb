2a46c9b1679f7dd42854f72ab0bbddf6bb51ef6a
Paul Allen <pea1@cornell.edu>
2017-03-30 11:06:05 -0400

    add aws dependency to metadata

--- cd-odsee/metadata.rb
+++ cd-odsee/metadata.rb
@@ -5,3 +5,4 @@ license           'All rights reserved'
 description       'Install Oracle Directory Server Enterprise Edition'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '0.1.0'
+depends           'aws'
