8c33a7e363d55bbedd449ada4ca6b4b5cfd28912
Paul Allen <pea1@cornell.edu>
2017-03-30 12:07:49 -0400

    add default attributes and begin using them

---
+++ cd-odsee/attributes/default.rb
@@ -0,0 +1,5 @@
+
+
+default[:odsee][:install_path]  = '/app/log/ldap/ds-7/'
+default[:odsee][:install_dir] = 'dsee7'
+
--- cd-odsee/recipes/default.rb
+++ cd-odsee/recipes/default.rb
@@ -7,12 +7,21 @@ package 'libstdc++-devel.i686'
 stack = search('aws_opsworks_stack').first
 aws_region = stack['region']
 
-%w[ /app /app/ldap /app/ldap/ds-7 ].each do |path|
-  directory '/app/ldap/ds-7' do
-    owner 'root'
-    group 'root'
-    mode '0755'
-  end
+# %w[ /app /app/ldap /app/ldap/ds-7 ].each do |path|
+#   directory path do
+#     owner 'root'
+#     group 'root'
+#     mode '0755'
+#   end
+# end
+
+install_dir = node[:odsee][:install_path]+node[:odsee][:install_dir]
+
+directory node[:odsee][:install_path] do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  recursive true
 end
 
 aws_s3_file '/tmp/ofm_odsee_linux_11.1.1.7.0_64_disk1_1of1.zip' do
@@ -27,10 +36,12 @@ end
 execute 'unzip_outer' do
   command 'unzip -o /tmp/ofm_odsee_linux_11.1.1.7.0_64_disk1_1of1.zip'
   cwd '/tmp'
+  creates '/tmp/ODSEE_ZIP_Distribution/sun-dsee7.zip'
 end
 
 execute 'unzip_inner' do
   command 'unzip -o /tmp/ODSEE_ZIP_Distribution/sun-dsee7.zip -d /app/ldap/ds-7/'
+  creates install_dir
 end
 
 aws_s3_file '/tmp/UnlimitedJCEPolicyJDK7.zip' do
@@ -44,6 +55,7 @@ end
 execute 'unzip_security' do
   command 'unzip -o /tmp/UnlimitedJCEPolicyJDK7.zip'
   cwd '/tmp'
+  creates '/tmp/UnlimitedJCEPolicy/US_export_policy.jar'
 end
 
 # cd /app/ldap/ds-7/dsee/jre/lib/security
@@ -54,7 +66,7 @@ end
 
 %w[ local_policy.jar US_export_policy.jar ].each do |target_file|
   remote_file target_file do
-    path '/app/ldap/ds-7/dsee7/jre/lib/security/'+target_file
+    path install_dir+'/jre/lib/security/'+target_file
     source 'file:///tmp/UnlimitedJCEPolicy/'+target_file
   end
 end
@@ -72,8 +84,9 @@ end
 # ***
 # http://docs.oracle.com/cd/E29127_01/doc.111170/e28967/dsccsetup-1m.htm
 execute 'ads-create' do
-  command '/app/ldap/ds-7/dsee7/bin/dsccsetup ads-create -w /tmp/password.txt'
-  not_if '/app/ldap/ds-7/dsee7/bin/dsccsetup status | grep "DSCC Registry has been created"'
+  command 'bin/dsccsetup ads-create -w /tmp/password.txt'
+  only_if 'bin/dsccsetup status | grep "DSCC Registry has been created"'
+  cwd install_dir
 end
 
 execute 'war-file-create' do
