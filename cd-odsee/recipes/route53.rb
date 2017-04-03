bdf6670a5fa5dff686af44cc21daf1ac238548e1
Paul Allen <pea1@cornell.edu>
2017-04-03 11:39:28 -0400

    debug route53 action

--- cd-odsee/recipes/route53.rb
+++ cd-odsee/recipes/route53.rb
@@ -4,7 +4,7 @@ server_name = 'aws'+node['odsee']['environment']+'ds'+instance['hostname']
 # Setup DNS
 ##################################################################
 route53_record "route53-config" do
-  name  server_name+'cs.cucloud.net'
+  name  server_name+'.cs.cucloud.net'
   value instance['public_ip']
   type  "A"
 
