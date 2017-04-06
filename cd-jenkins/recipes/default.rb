b3bc61e7bff923c2cdfe5c718d94bc8a1da106aa
nl85cit <ned.lacelle@cornell.edu>
2017-04-06 15:11:46 -0400

    modified EFS url for testing

--- cd-jenkins/recipes/default.rb
+++ cd-jenkins/recipes/default.rb
@@ -6,9 +6,9 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-# 2017-04-06 : nl85 - adding attribute for EFS mount 
+# 2017-04-06 : nl85 - adding attribute for EFS mount
 #
- 
+
 yum_package 'nfs-utils'
 
 user 'jenkins' do
@@ -28,8 +28,11 @@ directory '/var/jenkins_home' do
   action :create
 end
 
+# used by cs-jenkins
+#command "mount -t nfs4 -o nfsvers=4.1 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).fs-137fb25a.efs.us-east-1.amazonaws.com:/ /var/jenkins_home"
+
 execute "mount efs directory" do
-  command "mount -t nfs4 -o nfsvers=4.1 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).fs-137fb25a.efs.us-east-1.amazonaws.com:/ /var/jenkins_home"
+  command "mount -t nfs4 -o nfsvers=4.1 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).fs-a44df9ed.efs.us-east-1.amazonaws.com:/ /var/jenkins_home"
   retries 1
   retry_delay 5
   not_if "grep -qs '/var/jenkins_home' /proc/mounts"
