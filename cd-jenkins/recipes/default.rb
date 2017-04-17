ad9be074c8117f34c7dc05f5eca36d1aa5d517bc
Paul Allen <pea1@cornell.edu>
2017-04-17 17:13:18 -0400

    ensure that permissions are correct for newly created EFS volumes

--- cd-jenkins/recipes/default.rb
+++ cd-jenkins/recipes/default.rb
@@ -36,7 +36,7 @@ execute "mount efs directory" do
   retries 1
   retry_delay 5
   not_if "grep -qs '/var/jenkins_home' /proc/mounts"
-  notifies :run, 'directory[/var/jenkins_home]', :immediately
+  notifies :create, 'directory[/var/jenkins_home]', :immediately
 end
 
 service 'docker' do
