8dbf2e1f400a12a46e7c5a49ee8c8af7f60f51dd
Paul Allen <pea1@cornell.edu>
2017-04-20 15:07:58 -0400

    different approach for hostname

---
+++ cd-base/recipes/test.rb
@@ -0,0 +1,14 @@
+
+stack = search("aws_opsworks_stack").first
+#stack['name']
+instance = search("aws_opsworks_instance", "self:true").first
+#instance['hostname']
+
+execute 'set-hostname' do
+  command "hostname #{stack['name']}-#{instance['hostname']}"
+  not_if instance['hostname'].start_with?(stack['name'])
+end
+
+service 'rsyslog' do
+  action :restart
+end
