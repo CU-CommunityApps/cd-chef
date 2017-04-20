24824138ddaf36b2ec827012bf0fb0675779ed37
Paul Allen <pea1@cornell.edu>
2017-04-20 13:14:57 -0400

    add docker daemon.json config file template

---
+++ cd-jenkins/attributes/default.rb
@@ -0,0 +1,3 @@
+default['jenkins']['docker_daemon']['debug'] = true
+default['jenkins']['docker_daemon']['tls'] = false
+default['jenkins']['docker_daemon']['hosts'] = ["unix:///var/run/docker.sock"]
---
+++ cd-jenkins/recipes/docker-daemon.rb
@@ -0,0 +1,3 @@
+template '/etc/docker/daemon.json' do
+  source 'docker-daemon.json.erb'
+end
