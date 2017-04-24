deb1daa0950071709e837e0a090079704fbd50ff
eajfour <eaj.four@gmail.com>
2017-04-24 16:29:56 -0400

    added decrypted secret logic and file create and delete

--- cd-odsee/recipes/secrets.rb
+++ cd-odsee/recipes/secrets.rb
@@ -16,3 +16,37 @@ OpsWorksKMSSecrets.decrypt_attributes(region, node, 'agent_password')
 OpsWorksKMSSecrets.decrypt_attributes(region, node, 'dmadmin_password')
 
 # After decryption, node['admin_password']['secret_key_decrypted'] will contain the decrypted value of node['admin_password']['secret_key_encrypted'].
+
+# file '/tmp/admin_password.txt' do
+#   content node['admin_password']['secret_key_decrypted']
+#   mode '0755'
+#   owner 'root'
+#   group 'root'
+# end
+
+# file '/tmp/agent_password.txt' do
+#   content node['agent_password']['secret_key_decrypted']
+#   mode '0755'
+#   owner 'root'
+#   group 'root'
+# end
+
+# file '/tmp/dmadmin_password.txt' do
+#   content node['dmadmin_password']['secret_key_decrypted']
+#   mode '0755'
+#   owner 'root'
+#   group 'root'
+# end
+
+# cleanup
+# file '/tmp/admin_password.txt' do
+#   action :delete
+# end
+
+# file '/tmp/agent_password.txt' do
+#   action :delete
+# end
+
+# file '/tmp/dmadmin_password.txt' do
+#   action :delete
+# end
