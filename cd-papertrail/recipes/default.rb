ea93e0e6f3c90b7dfa95dc48298841be89d15e17
Paul Allen <pea1@cornell.edu>
2017-05-19 11:56:36 -0400

    fix node reference; change to node.default

--- cd-papertrail/recipes/default.rb
+++ cd-papertrail/recipes/default.rb
@@ -1,8 +1,8 @@
-include_recipe "remote_syslog2::default"
-
 stack = search("aws_opsworks_stack").first
 #stack['name']
 instance = search("aws_opsworks_instance", "self:true").first
 #instance['hostname']
 
-node['remote_syslog2']['config']['hostname'] = "#{stack['name']}-#{instance['hostname']}"
+node.default['remote_syslog2']['config']['hostname'] = "#{stack['name']}-#{instance['hostname']}"
+
+include_recipe "remote_syslog2::default"
