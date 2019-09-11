ce5cfae68adb7a5ef24ff99d564489f9523a03de
Paul Allen <pea1@cornell.edu>
2019-09-11 14:48:38 +0000

    remove odsee work

--- cd-odsee/recipes/default.rb
+++
@@ -1,7 +0,0 @@
-include_recipe "cd-odsee::setup_credentials"
-include_recipe "cd-odsee::odsee_server"
-include_recipe "cd-odsee::import_data"
-include_recipe "cd-odsee::tomcat"
-
-# Leave credentials in place during development
-# include_recipe "cd-odsee::cleanup"
